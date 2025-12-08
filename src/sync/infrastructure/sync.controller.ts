c23bf351a1959c068911704ed0a4ee2ad62698a3
Bohdan Kiselov <bandanovvs@gmail.com>
2025-12-08 23:01:12 +0200

    Fix error handling

--- src/sync/infrastructure/sync.controller.ts
+++ src/sync/infrastructure/sync.controller.ts
@@ -3,7 +3,6 @@ import { sendApi } from '../../shared/helpers/send-api.helper';
 import { SyncUserStatsUseCase } from '../application/use-cases/sync-user-stats.use-case';
 import { PostgresUserRepository } from './repositories/postgres-user.repository';
 import { SyncRequestDto, SyncResponseDto } from './dtos/sync.dto';
-import { logger } from '../../shared/logger';
 import { mapDtoToCommand, mapResultToDto } from './mappers/sync.mapper';
 import { EventService } from '../domain/services/event.service';
 
@@ -16,14 +15,9 @@ export async function createSyncController() {
   const syncUserStatsUseCase = new SyncUserStatsUseCase(postgresUserRepository, eventService);
 
   router.post('/', async (req: Request<unknown, unknown, SyncRequestDto>, res: Response<SyncResponseDto>) => {
-    try {
-      const command = mapDtoToCommand(req.body);
-      const result = await syncUserStatsUseCase.execute(command);
-      return sendApi(res, 200, 'OK', mapResultToDto(result));
-    } catch (err) {
-      logger.error(err);
-      return sendApi(res, 500, 'Internal Server Error');
-    }
+    const command = mapDtoToCommand(req.body);
+    const result = await syncUserStatsUseCase.execute(command);
+    return sendApi(res, 200, 'OK', mapResultToDto(result));
   });
 
   return router;
